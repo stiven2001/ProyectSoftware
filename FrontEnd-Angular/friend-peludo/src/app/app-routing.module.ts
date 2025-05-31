d240d32594b70d234115d71bb6a4b447df4e389d
stiven2001 <104127262+stiven2001@users.noreply.github.com>
2025-05-31 01:51:47 -0500

    Se adelanta Home.
    Falta Agg, Img Assets.

--- FrontEnd-Angular/friend-peludo/src/app/app-routing.module.ts
+++ FrontEnd-Angular/friend-peludo/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePublicComponent } from './home/home-public/home-public.component';
 
 
 const routes: Routes = [
   {
         path: '',
-        component: HomeComponent
+        component: HomePublicComponent
     },
 ];
 
